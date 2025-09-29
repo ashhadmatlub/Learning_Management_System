e29a1239bb6343478bc010f0ec925f750ea7f33a
ashhadmatlub <ashhadmatlub@gmail.com>
2025-09-29 18:00:52 +0500

    Input set

--- controller/App.java
+++ controller/App.java
@@ -1,14 +1,28 @@
 package controller;
 
+import java.util.Scanner;
+
 import data.student.Input;
 
 public class App {
     public void run() {
 
+        Scanner sc = new Scanner(System.in);
+        String cmd = sc.nextLine();
+
         try {
             Input data = new Input();
-            data.inputStudentData();
-            data.search();
+
+            System.out.println("Welcome to the LMS. Please review and follow the instructions properly outlined below");
+
+            System.out.println("Enter the command ( add ) to insert a student into the list.");
+            System.out.println("Enter the command ( search ) to insert a student into the list.");
+
+            if (cmd.trim().equalsIgnoreCase("add")) {
+                data.inputStudentData();
+            } else if (cmd.trim().equalsIgnoreCase("search")) {
+                data.search();
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
